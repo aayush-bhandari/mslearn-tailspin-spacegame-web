98935e5225aea9a5f758a1781aafbd5d1b250ad4
Aayush Bhandari <aabhandari@microsoft.com>
2022-09-30 16:53:44 -0500

    CI modifications

---
+++ Tailspin.SpaceGame.Web/merge.js
@@ -0,0 +1,25 @@
+const fs = require('fs');
+const jsonsInDir = fs.readdirSync('./Aayush')
+let mergedString = '{'
+
+jsonsInDir.forEach(file => {
+    const fileData = fs.readFileSync(path.join('./Aayush', file));
+    const json = JSON.parse(fileData.toString());
+    for (const [key, value] of Object.entries(json)) {
+        console.log(`${key}: ${value}`);
+        mergedString = `${mergedString}"${key}": ${value}},`
+      }
+});
+mergedString = mergedString.slice(0,-1)+'}'
+const mergedJson = JSON.stringify(mergedString);
+
+// write file to disk
+fs.writeFile('./DownloadedArtifacts/versions.json', mergedJson, 'utf8', (err) => {
+
+    if (err) {
+        console.log(`Error writing file: ${err}`);
+    } else {
+        console.log(`File is written successfully!`);
+    }
+
+});
