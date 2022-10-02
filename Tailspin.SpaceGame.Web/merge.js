eed30b5bff56908da9de18665efb804bc15f1f44
Aayush Bhandari <aabhandari@microsoft.com>
2022-10-02 14:04:51 -0500

    CI modifications

--- Tailspin.SpaceGame.Web/merge.js
+++ Tailspin.SpaceGame.Web/merge.js
@@ -8,12 +8,12 @@ jsonsInDir.forEach(file => {
     const json = JSON.parse(fileData.toString());
     for (const [key, value] of Object.entries(json)) {
         console.log(`${key}: ${value}`);
-        mergedString = `${mergedString}"${key}": ${value},`
+        mergedString = `${mergedString} ${key}: ${value},`
       }
 });
 mergedString = mergedString.slice(0,-1)+'}'
 console.log("mergedString: "+mergedString);
-const mergedJson = JSON.stringify(mergedString);
+const mergedJson = JSON.stringify(mergedString, null, 4);
 
 // write file to disk
 fs.writeFile('/home/vsts/work/1/s//DownloadedArtifacts/versions.json', mergedJson, 'utf8', (err) => {
