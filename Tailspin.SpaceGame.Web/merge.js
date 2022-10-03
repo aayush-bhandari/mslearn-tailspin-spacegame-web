a0b5a1a06aef80466ad7131cf42c524166c5f7a2
Aayush Bhandari <aabhandari@microsoft.com>
2022-10-02 23:06:38 -0500

    CI modifications

--- Tailspin.SpaceGame.Web/merge.js
+++ Tailspin.SpaceGame.Web/merge.js
@@ -1,18 +1,20 @@
 const fs = require('fs');
 const path = require('path')
-const jsonsInDir = fs.readdirSync('/home/vsts/work/1/s/Tailspin.SpaceGame.Web/Aayush/')
+//const jsonDir = '/home/vsts/work/1/s/Tailspin.SpaceGame.Web/Aayush/'
+const jsonDir = '$(Build.SourcesDirectory)/Tailspin.SpaceGame.Web/CDVersion/'
+const jsonsInDir = fs.readdirSync(jsonDir)
 let mergedString = '{'
 
 jsonsInDir.forEach(file => {
-    const fileData = fs.readFileSync(path.join('/home/vsts/work/1/s/Tailspin.SpaceGame.Web/Aayush/', file));
+    const fileData = fs.readFileSync(path.join(jsonDir, file));
     const json = JSON.parse(fileData.toString());
     for (const [key, value] of Object.entries(json)) {
         console.log(`${key}: ${value}`);
-        mergedString = `${mergedString} "${key}": ${value},`
+        mergedString = `${mergedString} "${key}": "${value}",`
       }
 });
 mergedString = mergedString.slice(0,-1)+'}'
-console.log("mergedString: "+mergedString);
+console.log("mergedString: ", mergedString);
 //const mergedJson = JSON.stringify(mergedString, null, 4);
 
 // write file to disk
