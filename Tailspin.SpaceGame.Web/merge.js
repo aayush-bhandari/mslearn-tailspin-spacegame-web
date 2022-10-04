9d4956c99742cd40a45ca8b83b9940c88ef8e6f0
Aayush Bhandari <aabhandari@microsoft.com>
2022-10-03 23:46:43 -0500

    CI modifications

--- Tailspin.SpaceGame.Web/merge.js
+++ Tailspin.SpaceGame.Web/merge.js
@@ -3,27 +3,33 @@ const path = require('path')
 //const jsonDir = '/home/vsts/work/1/s/Tailspin.SpaceGame.Web/Aayush/'
 const jsonDir = '/home/vsts/work/1/s/Tailspin.SpaceGame.Web/CDVersion/'
 const jsonsInDir = fs.readdirSync(jsonDir)
-let mergedString = '{'
 
-jsonsInDir.forEach(file => {
-    const fileData = fs.readFileSync(path.join(jsonDir, file));
-    const json = JSON.parse(fileData.toString());
-    for (const [key, value] of Object.entries(json)) {
-        console.log(`${key}: ${value}`);
-        mergedString = `${mergedString} "${key}": "${value}",`
-      }
-});
-mergedString = mergedString.slice(0,-1)+'}'
-console.log("mergedString: ", mergedString);
-//const mergedJson = JSON.stringify(mergedString, null, 4);
+function merge(){
+    let mergedString = '{'
+    jsonsInDir.forEach(file => {
+        const fileData = fs.readFileSync(path.join(jsonDir, file));
+        const json = JSON.parse(fileData.toString());
+        for (const [key, value] of Object.entries(json)) {
+            console.log(`${key}: ${value}`);
+            mergedString = `${mergedString} "${key}": "${value}",`
+        }
+    });
+    mergedString = mergedString.slice(0,-1)+'}'
+    console.log("mergedString: ", mergedString);
+    //const mergedJson = JSON.stringify(mergedString, null, 4);
 
-// write file to disk
-fs.writeFile('/home/vsts/work/1/s/DownloadedArtifacts/versions.json', mergedString, 'utf8', (err) => {
+    // write file to disk
+    fs.writeFile('/home/vsts/work/1/s/DownloadedArtifacts/versions.json', mergedString, 'utf8', (err) => {
 
-    if (err) {
-        console.log(`Error writing file: ${err}`);
-    } else {
-        console.log(`File is written successfully!`);
-    }
+        if (err) {
+            console.log(`Error writing file: ${err}`);
+        } else {
+            console.log(`File is written successfully!`);
+        }
 
-});
+    });
+}
+
+(async () => {
+    merge();
+})();
